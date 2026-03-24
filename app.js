711a5a95b464bd04f46bb888b6a1f27f4e745e90
Deimar <54513972+DeimarVR@users.noreply.github.com>
2026-03-24 09:02:19 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks() {
+  const list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+
+    if (task.completed) li.classList.add("completed");
+
+    li.innerHTML = `
+      <span onclick="toggleTask(${index})">${task.text}</span>
+      <button class="delete" onclick="deleteTask(${index})">X</button>
+    `;
+
+    list.appendChild(li);
+  });
+}
+
+function addTask() {
+  const input = document.getElementById("taskInput");
+  if (!input.value) return;
+
+  tasks.push({ text: input.value, completed: false });
+  input.value = "";
+
+  saveTasks();
+  renderTasks();
+}
+
+function toggleTask(index) {
+  tasks[index].completed = !tasks[index].completed;
+  saveTasks();
+  renderTasks();
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  saveTasks();
+  renderTasks();
+}
+
+// cargar tareas al inicio
+renderTasks();
